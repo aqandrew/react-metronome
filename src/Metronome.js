1e88c4f748b3bb786c56feb5df26694c41e433ac
dawneraq <dawneraq@gmail.com>
2020-01-31 16:38:40 -0800

    add Bootstrap and readonly bpm slider

--- src/Metronome.js
+++ src/Metronome.js
@@ -3,9 +3,19 @@ import './Metronome.css';
 
 class Metronome extends Component {
   render() {
+    let bpm = 140;
+    let playing = false;
+
     return (
-      <div className="metronome">
-        I'm a metronome!
+      <div className="container">
+        <h1>Metronome</h1>
+        <div className="metronome">
+          <div className="bpm-slider">
+            <h2>{bpm} BPM</h2>
+            <input type="range" min="60" max="240" value={bpm} className="form-control-range w-100 m-4" />
+          </div>
+          <button className="btn btn-primary btn-lg">{playing ? 'Stop' : 'Start'}</button>
+        </div>
       </div>
     );
   }
